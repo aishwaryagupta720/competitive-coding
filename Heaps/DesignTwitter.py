e96e0c097fb2024272883917e4010992336a0415
aishwarya-drishti <aishwaryagupta720@gmail.com>
2024-05-02 13:57:28 -0700

    2 May

---
+++ Heaps/DesignTwitter.py
@@ -0,0 +1,52 @@
+class Twitter:
+
+    def __init__(self):
+        self.tweetcount=0
+        self.follows=defaultdict(set)
+        self.tweets= defaultdict(list)
+        
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweetcount+=1
+        self.tweets[userId].append([-1*self.tweetcount,tweetId])
+        print(self.tweets)
+        
+    def getNewsFeed(self, userId: int) -> List[int]:
+        heap=self.tweets[userId][:]
+        for user in self.follows[userId]:
+            heap=heap+self.tweets[user]
+        heapq.heapify(heap)
+        ans=[]
+        for i in range(10):
+            if not heap:
+                break
+            ans.append(heapq.heappop(heap)[1])
+        return ans
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.follows[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.follows[followerId]:
+            self.follows[followerId].remove(followeeId)
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
+
+
+
+
+# Design a simplified version of Twitter where users can post tweets, follow/unfollow another user, and is able to see the 10 most recent tweets in the user's news feed.
+
+# Implement the Twitter class:
+
+# Twitter() Initializes your twitter object.
+# void postTweet(int userId, int tweetId) Composes a new tweet with ID tweetId by the user userId. Each call to this function will be made with a unique tweetId.
+# List<Integer> getNewsFeed(int userId) Retrieves the 10 most recent tweet IDs in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user themself. Tweets must be ordered from most recent to least recent.
+# void follow(int followerId, int followeeId) The user with ID followerId started following the user with ID followeeId.
+# void unfollow(int followerId, int followeeId) The user with ID followerId started unfollowing the user with ID followeeId.
+ 
